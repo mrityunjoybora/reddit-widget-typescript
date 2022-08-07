86f1078cb7b97861430d596338908bc56f2ce409
mrityunjoybora <mjborabiz@gmail.com>
2022-08-07 20:06:27 +0530

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+interface ResponseData {
+  data: Data;
+}
+interface Data {
+  children: Child[];
+}
+interface Child {
+  kind: string;
+  data: RedditPostData;
+}
+interface RedditPost {
+  data: RedditPostData;
+};
+interface RedditPostData {
+  title: string;
+  url: string;
+  author: string;
+  id: string;
+  stickied: boolean;
+};
+
+// Render each post
+function renderPost(post: RedditPost) {
+  const {
+    data: { title, url, author, id },
+  } = post;
+  const authorUrl = `https://www.reddit.com/u/${author}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className="reddit_widget__post" key={id}>
+      <div className="reddit_widget__posted_by">
+        posted by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={authorUrl}
+          className="reddit_widget__posted_by"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          u/{author}
         </a>
-      </header>
+      </div>
+      <a
+        href={url}
+        className="reddit_widget__title"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {title}
+      </a>
     </div>
   );
 }
+
+// Filter, since reddit always returns stickied posts up top
+function nonStickiedOnly(post: RedditPost) {
+  return !post.data.stickied;
+}
+
+const App: React.FC<{ domElement: Element }> = ({ domElement }) => {
+  const subreddit = domElement.getAttribute("data-subreddit");
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [data, setData] = useState<Child[]>([]);
+
+  useEffect(() => {
+    // Fetch data from reddit
+    setLoading(true);
+    fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then((response) => response.json())
+      .then((data: ResponseData) => {
+        setLoading(false);
+        setData(data.data.children.slice(0, 10));
+      })
+      .catch((e) => {
+        console.log(e);
+        setLoading(false);
+        setError("error fetching from reddit");
+      });
+  }, [subreddit]);
+
+  return (
+    <div className="reddit_widget__app">
+      <h1 className="reddit_widget__header">
+        Latest posts in{" "}
+        <a href={`https://reddit.com/r/${subreddit}`} rel="noopener noreferrer">
+          /r/{subreddit}
+        </a>
+      </h1>
+      <div className="reddit_widget__inner">
+        {loading && "Loading..."}
+        {error && error}
+        {!!data.length && data.filter(nonStickiedOnly).map(renderPost)}
+      </div>
+      <p className="reddit_widget__powered_by">
+        This widget is powered by{" "}
+        <a
+          href="https://javascriptpros.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          JavaScriptPros.com
+        </a>
+      </p>
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const WidgetDivs = document.querySelectorAll('.reddit_widget')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+WidgetDivs.forEach(Div => {
+
+  const root = ReactDOM.createRoot(
+      Div
+    );
+
+    root.render(
+    <React.StrictMode>
+      <App domElement={Div}/>
+    </React.StrictMode>
+  );
+})
+
+ 
+
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
